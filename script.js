d06bb97c23e5eacd735592f0561c190c68f13aef
ezekiel charo <ezekiel3499@gmail.com>
2023-09-03 22:45:15 +0300

    Mobile menu toggle

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const activeFarmers = document.querySelector("#active-farmers");
 const years = document.querySelector("#num-years");
 
-let farmersCount = 0;
-let yearsCount = 0;
-const totalFarmers = 400;
-const totalYears = 23;
+if (activeFarmers && years) {
+  let farmersCount = 0;
+  let yearsCount = 0;
+  const totalFarmers = 400;
+  const totalYears = 23;
 
-const interval = setInterval(() => {
-  farmersCount += 2;
-  yearsCount++;
+  const interval = setInterval(() => {
+    farmersCount += 2;
+    yearsCount++;
 
-  activeFarmers.textContent = `${farmersCount}+`;
+    activeFarmers.textContent = `${farmersCount}+`;
 
-  if (yearsCount <= totalYears) {
-    years.textContent = `${yearsCount}+`;
-  }
+    if (yearsCount <= totalYears) {
+      years.textContent = `${yearsCount}+`;
+    }
 
-  if (farmersCount === totalFarmers) {
-    clearInterval(interval);
-  }
-}, 1);
+    if (farmersCount === totalFarmers) {
+      clearInterval(interval);
+    }
+  }, 1);
+}
+
+const menu = document.querySelector(".menu");
+const menuToggle = document.querySelector(".toggle");
+let backdrop;
+
+menuToggle.addEventListener("click", (e) => {
+  document.body.classList.add("backdrop");
+  backdrop = document.querySelector(".backdrop");
+  menu.style.display = "flex";
+
+  const removeBackdrop = (ev) => {
+    if (!ev) {
+      return;
+    }
+
+    if (ev.target.className === "menu") {
+      return;
+    }
+
+    document.body.classList.remove("backdrop");
+    menu.style.display = "none"; // Hide menu
+    backdrop.removeEventListener("click", removeBackdrop);
+  };
+
+  backdrop.addEventListener("click", removeBackdrop);
+  e.stopPropagation(); // IMPORTANT to avoid menu beng hidden immediately
+});
